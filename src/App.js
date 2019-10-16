3dc01b958e6ead61c4d254f25532255ce2aadeda
jj-gaardbo <jgaard16@gmail.com>
2019-10-16 22:46:31 +0200

    Added stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketch';
+import DragAndDrop from "./components/DragAndDrop";
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {color:[Math.random()*255, Math.random()*255, Math.random()*255]};
-        this.randomColor = this.randomColor.bind(this);
+    constructor(props){
+        super(props);
+        this.state = {
+            file: null,
+            hasFile: false
+        };
     }
 
-    randomColor(){
-        this.setState({color:[Math.random()*255, Math.random()*255, Math.random()*255]}
-        )
+    handleDrop = (files) => {
+        if(this.state.files.length > 0){
+            this.setState({hasFile: true})
+            this.setState({file: files})
+        }
     }
 
     render() {
         return (
             <div className="container-fluid">
-                <button className="btn btn-primary btn-lg" onClick={this.randomColor}>Learn more</button>
-                <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
+                {!this.state.hasFile ? (
+                <DragAndDrop handleDrop={this.handleDrop}>
+                    <div style={{height: 300, width: 250, backgroundColor: '#ffffff'}}>
+                        {/*{this.state.file.map((file) =>
+                            <div key={i}>{file}</div>
+                        )}*/}
+                    </div>
+                </DragAndDrop>
+                ) : (
+                    <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
+                )}
             </div>
         );
     }
