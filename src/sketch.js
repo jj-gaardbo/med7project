e07d09f553273004f9dd60f7f333d153d774b55f
jj-gaardbo <jgaard16@gmail.com>
2019-10-16 09:35:37 +0200

    Added repo in package

--- src/sketch.js
+++ src/sketch.js
@@ -7,15 +7,15 @@ export default function sketch(p){
     p.setup = () => {
         p.createCanvas(1360, 916);
         bg = p.loadImage(image);
-    }
+    };
 
     p.draw = () => {
         p.background(bg);
         p.ellipse(150, 100, 100, 100);
-    }
+    };
 
     p.myCustomRedrawAccordingToNewPropsHandler = (newProps) => {
         if(canvas) //Make sure the canvas has been created
             p.fill(newProps.color);
-    }
+    };
 }
